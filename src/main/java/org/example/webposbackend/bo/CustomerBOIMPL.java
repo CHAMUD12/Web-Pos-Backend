361c44c816b013d61980d7cf44eb2bf6310a0929
CHAMUD12 <chamudgamage@gmail.com>
2024-08-07 11:20:53 +0530

    customer saved

---
+++ src/main/java/org/example/webposbackend/bo/CustomerBOIMPL.java
@@ -0,0 +1,29 @@
+package org.example.webposbackend.bo;
+
+import org.example.webposbackend.dao.CustomerDAOImpl;
+import org.example.webposbackend.dto.CustomerDTO;
+
+import java.sql.Connection;
+
+public class CustomerBOIMPL implements CustomerBO {
+    @Override
+    public String saveCustomer(CustomerDTO customer, Connection connection) throws Exception {
+        var customerDAOImpl = new CustomerDAOImpl();
+        return customerDAOImpl.saveCustomer(customer, connection);
+    }
+
+    @Override
+    public boolean updateCustomer(String id, CustomerDTO customer, Connection connection) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomer(String id, Connection connection) throws Exception {
+        return false;
+    }
+
+    @Override
+    public CustomerDTO getCustomer(String id, Connection connection) throws Exception {
+        return null;
+    }
+}
---
+++ src/main/java/org/example/webposbackend/dao/CustomerDAOImpl.java
@@ -0,0 +1,52 @@
+package org.example.webposbackend.dao;
+
+import org.example.webposbackend.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public final class CustomerDAOImpl implements CustomerDAO {
+    public static final String SAVE_CUSTOMER = "INSERT INTO customer (id, name, address, mobile) VALUES (?, ?, ?, ?)";
+    public static final String GET_CUSTOMER = "SELECT * FROM customer WHERE id = ?";
+    public static final String UPDATE_CUSTOMER = "UPDATE customer SET name = ?, address = ?, mobile = ?, WHERE id = ?";
+    public static final String DELETE_CUSTOMER = "DELETE FROM customer WHERE id = ?";
+    public static final String SEARCH_CUSTOMER = "SELECT * FROM customer WHERE id = ?";
+
+    @Override
+    public String saveCustomer(CustomerDTO customer, Connection connection) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement(SAVE_CUSTOMER)) {
+            ps.setString(1, customer.getId());
+            ps.setString(2, customer.getName());
+            ps.setString(3, customer.getAddress());
+            ps.setString(4, customer.getMobile());
+            if (ps.executeUpdate() != 0) {
+                return "Customer saved successfully";
+            } else {
+                return "Failed to save Customer";
+            }
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean updateCustomer(String id, CustomerDTO customer, Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomer(String id, Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean searchCustomer(String id, Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public CustomerDTO getCustomer(String id, Connection connection) throws SQLException {
+        return null;
+    }
+}
